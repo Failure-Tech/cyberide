f60c464b62c99745a0881960d3f7d95f267d2d2c
Gautham <korrapatigautham@gmail.com>
2025-09-30 19:00:50 -0700

    updated env + mdx finished docs

--- cyberide/.source/index.ts
+++ cyberide/.source/index.ts
@@ -1,6 +1,7 @@
 // @ts-nocheck -- skip type checking
-import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1758744563778"
+import * as docs_1 from "../content/docs/index.mdx?collection=docs&hash=1758744563778"
+import * as docs_0 from "../content/docs/getting-started.mdx?collection=docs&hash=1758744563778"
 import { _runtime } from "fumadocs-mdx"
 import * as _source from "../source.config"
-export const docs = _runtime.doc<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"C:/Users/korra/Documents/condrx/cyberide/cyberide/content/docs/index.mdx"}, data: docs_0 }]);
+export const docs = _runtime.doc<typeof _source.docs>([{ info: {"path":"getting-started.mdx","absolutePath":"C:/Users/korra/Documents/condrx/cyberide/cyberide/content/docs/getting-started.mdx"}, data: docs_0 }, { info: {"path":"index.mdx","absolutePath":"C:/Users/korra/Documents/condrx/cyberide/cyberide/content/docs/index.mdx"}, data: docs_1 }]);
 export const meta = _runtime.meta<typeof _source.meta>([]);
